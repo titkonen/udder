df61407e873354b6217b2a537f6c5545a7d7a56c
Toni Itkonen <toni.itkonen@icloud.com>
2021-06-15 08:50:13 +0300

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Toni Itkonen on 15.6.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
